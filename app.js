68304017e224ce0897491f6a6ded651e4f7797d8
Brandonn Andrés Cruz López <brandonn9898@gmail.com>
2021-02-02 17:00:20 -0500

    Running server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`);
+});
